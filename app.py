1292d74628b9cc81e7fa52c6fe2b92ae06363546
Dan Benson <dbenson3091@gmail.com>
2021-09-17 14:22:43 -0500

    added html template, updated pet model

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 """Flask app for adopt app."""
 
-from flask import Flask
-
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, TextAreaField, SelectField 
 from flask_debugtoolbar import DebugToolbarExtension
 
-from models import db, connect_db
+
+from models import db, connect_db, Pet
 
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = "secret"
-
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///adopt"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+app.config['SECRET_KEY'] = "secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 connect_db(app)
+
+# Don't forget to db.drop_all() incase of errors 
 db.create_all()
 
 # Having the Debug Toolbar show redirects explicitly is often useful;
@@ -22,3 +27,11 @@ db.create_all()
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
+
+@app.get('/') 
+def render_pet_list(): 
+    """Render pet_list"""
+
+    pets = Pet.query.all()
+
+    return render_template('', pets=pets)
--- models.py
+++ models.py
@@ -13,3 +13,23 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+class Pet(db.Model):
+    """Pet Model"""
+
+    __tablename__ = "pets"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    name = db.Column(db.String(50),
+                           nullable=False)
+    species = db.Column(db.String(50),
+                          nullable=False)
+    photo_url = db.Column(db.String(), default='', nullable=False)
+    age = db.Column(db.String(), nullable=False)
+    # Make note: need a drop when it comes to this selection
+    notes = db.Column(db.String(), nullable=True) 
+    # ASK should make true when previous is false? 
+    available = db.Column(db.Boolean(), nullable=False, default=True) 
+
